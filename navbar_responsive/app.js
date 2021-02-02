4a303b470bce5475f3e53f944f016e23b5614c33
Eric Alberto Costa e Costa <71726316+ericcostac@users.noreply.github.com>
2021-02-02 19:27:10 -0300

    Implementando o JavaScript

---
+++ navbar_responsive/app.js
@@ -0,0 +1,13 @@
+const itens = document.querySelector('.itens');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+
+itens.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+
+    links.forEach(link => {
+        link.classList.toggle('fade');
+    });
+}); 
+
